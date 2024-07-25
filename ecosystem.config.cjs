6f7ea50b2685cc0649d952236d69bb507e43bdac
Simon Carr <simonjcarr@gmail.com>
2024-07-25 11:20:53 +0100

    update pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,16 +8,16 @@ module.exports = {
             script: './.output/server/index.mjs'
         }
     ],
-    // deploy: {
-    //     // "production" is the environment name
-    //     production: {
-    //         user: 'simon',
-    //         host: ['65.21.242.43'],
-    //         ref: 'origin/main',
-    //         repo: 'git@github.com:simonjcarr/carrandson.git',
-    //         ssh_options: ['ForwardAgent=yes'],
-    //         path: '/home/simon/apps/carrandson',
-    //         'post-deploy': 'npm install && npm build && pm2 startOrRestart ecosystem.config.js --env production'
-    //     }
-    // }
+    deploy: {
+        // "production" is the environment name
+        production: {
+            user: 'simon',
+            host: ['65.21.242.43'],
+            ref: 'origin/main',
+            repo: 'git@github.com:simonjcarr/carrandson.git',
+            ssh_options: ['ForwardAgent=yes'],
+            path: '/home/simon/apps/carrandson',
+            'post-deploy': 'npm install && npm build && pm2 startOrRestart ecosystem.config.js --env production'
+        }
+    }
 }
