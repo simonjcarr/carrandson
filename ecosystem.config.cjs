3ef81a5a0cc3dc05611755ea4a237156c5912d10
Simon Carr <simonjcarr@gmail.com>
2024-07-25 11:18:39 +0100

    update pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,23 @@
 module.exports = {
     apps: [
         {
-            name: 'NuxtAppName',
+            name: 'CarrandSon',
             port: '3000',
             exec_mode: 'cluster',
             instances: 'max',
             script: './.output/server/index.mjs'
         }
-    ]
+    ],
+    // deploy: {
+    //     // "production" is the environment name
+    //     production: {
+    //         user: 'simon',
+    //         host: ['65.21.242.43'],
+    //         ref: 'origin/main',
+    //         repo: 'git@github.com:simonjcarr/carrandson.git',
+    //         ssh_options: ['ForwardAgent=yes'],
+    //         path: '/home/simon/apps/carrandson',
+    //         'post-deploy': 'npm install && npm build && pm2 startOrRestart ecosystem.config.js --env production'
+    //     }
+    // }
 }
